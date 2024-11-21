c5abe7ba04fcd479232a2835b45f2fa1aedf8f9e
FaronicsProServices <proservices@faronics.com>
2024-11-21 22:41:33 +0530

    Create ListAllEventLogs.ps1

---
+++ ListAllEventLogs.ps1
@@ -0,0 +1,2 @@
+# The Get-WinEvent -ListLog * command in PowerShell gives a detailed list of all event logs available on your computer, including both traditional logs and newer logs that are more specific to modern Windows features.
+Get-WinEvent -ListLog *
